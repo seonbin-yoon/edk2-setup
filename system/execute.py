56328771d4fd6f9a7a832d6eba6f7ee406b59a7e
Seonbin Yoon <seonbin.yoon0@gmail.com>
2026-06-13 13:16:27 +0900

    변수 이름 개선, 예외 이름 추가

--- system/execute.py
+++ system/execute.py
@@ -1,11 +1,15 @@
 import subprocess
 
 from utils import color_print, datatype
+from utils._except import RunExcept
 from utils.color_print import Color
 
 
-def shell_run(tasks: list[datatype.Task], task_contents: datatype.TaskContexts):
-    for num, task in enumerate(tasks):
+def shell_run(
+        shell_tasks: list[datatype.ShellTask],
+        task_contents: datatype.TaskContexts):
+
+    for num, task in enumerate(shell_tasks):
         color_print.write(
             f"\r[{num + 1}/{task_contents.total_num}] {task['Message']} -> 진행중..",
             Color.BLUE, end="")
@@ -18,7 +22,7 @@ def shell_run(tasks: list[datatype.Task], task_contents: datatype.TaskContexts):
                 check=True
                 )
         except subprocess.CalledProcessError as error:
-            raise RuntimeError(
+            raise RunExcept.FailedRunError(
                 f"\r[{num + 1}/{task_contents.total_num}] "\
                 f"{task['Message']} -> 실패.{"":<5}\n"\
                 f"{error}") from error
@@ -28,9 +32,9 @@ def shell_run(tasks: list[datatype.Task], task_contents: datatype.TaskContexts):
             Color.GREEN)
 
 def func_run(
-    func_tasks: list[datatype.Function],
+    func_tasks: list[datatype.FunctionTask],
     task_contents: datatype.TaskContexts,
-    context: datatype.Contexts):
+    program_context: datatype.Contexts):
 
     for num, func_task in enumerate(func_tasks):
         func = func_task["Func"]
@@ -40,9 +44,9 @@ def func_run(
             f"{func_task['Message']} -> 진행중..",
             Color.BLUE, end="")
         try:
-            func(context)
+            func(program_context)
         except (FileNotFoundError, PermissionError) as error:
-            raise RuntimeError(
+            raise RunExcept.FailedRunError(
                 f"\r[{(task_contents.shell_task_num + num) + 1}/"\
                 f"{task_contents.total_num}] "\
                 f"{func_task['Message']} -> 실패.{"":<5}\n"
